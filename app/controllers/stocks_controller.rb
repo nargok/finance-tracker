2142e3140e99dbe5c4a74f4f847b73170c599d1e
Ryoichi Gokan <nargok@gmail.com>
2018-05-12 15:03:56 +0900

    code refactaring stocks_controller

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,16 +1,11 @@
 class StocksController < ApplicationController
   def search
-    if params[:stock].present?
-      @stock = Stock.new_form_lookup(params[:stock])
-      if @stock
-        render partial: 'users/result'
-      else
-        flash.now[:danger] = "You have entered an incerrect symbol"
-        render partial: 'users/result'
-      end
-    else
+    if params[:stock].blank?
       flash.now[:danger] = "You have entered an empty search string"
-      render partial: 'users/result'
+    else
+      @stock = Stock.new_form_lookup(params[:stock])
+      flash.now[:danger] = "You have entered an incerrect symbol" unless @stock
     end
+    render partial: 'users/result'
   end
 end
