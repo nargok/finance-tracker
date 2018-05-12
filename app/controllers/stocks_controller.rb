99b9b31e7973a62d7453255e66fd28dca9b2c9c6
Ryoichi Gokan <nargok@gmail.com>
2018-05-12 15:50:29 +0900

    association between stock model & user model

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -3,7 +3,7 @@ class StocksController < ApplicationController
     if params[:stock].blank?
       flash.now[:danger] = "You have entered an empty search string"
     else
-      @stock = Stock.new_form_lookup(params[:stock])
+      @stock = Stock.new_from_lookup(params[:stock])
       flash.now[:danger] = "You have entered an incerrect symbol" unless @stock
     end
     render partial: 'users/result'
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,10 +1,16 @@
 class Stock < ApplicationRecord
-  def self.new_form_lookup(ticker_symbol)
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+  def self.new_from_lookup(ticker_symbol)
     begin
       looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
       new(name: looked_up_stock.company_name, ticker: looked_up_stock.symbol, last_price: looked_up_stock.latest_price)
     rescue Exception => e
       return nil
     end
+  end
+
+  def find_by_ticker(ticker_symbol)
+    where(ticker: ticker_symbol).first
   end
 end
