229326c0a72cbe5a5097688c912df99b246d2911
Ryoichi Gokan <nargok@gmail.com>
2018-05-27 18:47:38 +0900

    add firtst_name & last_name in user_registration form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controller => {:registrations => 'users/registrations'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'users#my_portfolio'
   get 'my_portfolio', to: 'users#my_portfolio'
