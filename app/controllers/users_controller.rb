c5f8047caea84c78d1d0d98265b2384d6d8bf7f7
Ryoichi Gokan <nargok@gmail.com>
2018-06-07 23:02:54 +0900

    search friends process & flash message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,12 @@ class UsersController < ApplicationController
   end
 
   def search
-
+    if params[:search_param].blank?
+      flash.now[:danger] = "You have entered an empty search string"
+    else
+      @users = User.search(params[:search_param])
+      flash.now[:danger] = "No users match this search criteria" if @users.blank?
+    end
+    render partial: 'friends/result'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users, :controller => {:registrations => 'users/registrations'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources 'user_stocks', only: [:create, :destroy]
+  resources :users, only: [:show]
+  resources :friendships
+
   root 'users#my_portfolio'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stocks', to: 'stocks#search'
-  resources 'user_stocks', only: [:create, :destroy]
   get 'my_friends', to: 'users#my_friends'
-  resources :users, only: [:show]
-  resources :friendships
-  get 'search_friends', to: 'user#search'
+  get 'search_friends', to: 'users#search'
 end
